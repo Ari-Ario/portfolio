522b033783b600cc62c163361e82c3f3581f39eb
Ari <ari@Aris-MacBook-Pro.local>
2024-03-11 22:39:50 +0100

    sth added

--- www/public/film/index-film.php
+++ www/public/film/index-film.php
@@ -23,6 +23,11 @@
 
         <main id="main-film">
 
+        <!-- <div class="w-layout-grid grid-project-b"><a href="#" id="" class="lightbox-link type-b w-inline-block w-lightbox" aria-label="open lightbox" aria-haspopup="dialog">
+                    <div data-poster-url="https://assets-global.website-files.com/640d124abc45fd23e3362055/640ecdd4ec61394f33311a88_1_Crisp TVC on Vimeo-poster-00001.jpg" data-video-urls="https://assets-global.website-files.com/640d124abc45fd23e3362055/640ecdd4ec61394f33311a88_1_Crisp TVC on Vimeo-transcode.mp4,https://assets-global.website-files.com/640d124abc45fd23e3362055/640ecdd4ec61394f33311a88_1_Crisp TVC on Vimeo-transcode.webm" data-autoplay="true" data-loop="true" data-wf-ignore="true" class="background-video no-image w-background-video w-background-video-atom"><video id="" autoplay="" loop="" style="background-image:url(&quot;https://assets-global.website-files.com/640d124abc45fd23e3362055/640ecdd4ec61394f33311a88_1_Crisp TVC on Vimeo-poster-00001.jpg&quot;)" muted="" playsinline="" data-wf-ignore="true" data-object-fit="cover"><source src="https://assets-global.website-files.com/640d124abc45fd23e3362055/640ecdd4ec61394f33311a88_1_Crisp TVC on Vimeo-transcode.mp4" data-wf-ignore="true"><source src="https://assets-global.website-files.com/640d124abc45fd23e3362055/640ecdd4ec61394f33311a88_1_Crisp TVC on Vimeo-transcode.webm" data-wf-ignore="true"></video>
+                    </div> -->
+
+
             <div id="" data-w-id="" style="width: 100%;" class="project-hero-b">
                 <div style="transform: translate3d(0%, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;" class="block-project-hero">
                     <video src="" controls autoplay>
