beb322a8402b175902b5362586f6dc026091b4d7
Ari <ari@Aris-MacBook-Pro.local>
2024-03-18 19:05:39 +0100

    film site finished

--- www/public/film/index-film.php
+++ www/public/film/index-film.php
@@ -18,7 +18,7 @@
 
     <section id="film-container">
         <aside id="aside-film">
-            <h1>The power of <strong>visual storytelling</strong></h1>
+            <div id="aside-desc"><h1>Meeting of: <br>. Visualization <br>. Storytelling <br>. Design</h1></div>
         </aside>
 
         <main id="main-film">
@@ -27,25 +27,24 @@
                     <div data-poster-url="https://assets-global.website-files.com/640d124abc45fd23e3362055/640ecdd4ec61394f33311a88_1_Crisp TVC on Vimeo-poster-00001.jpg" data-video-urls="https://assets-global.website-files.com/640d124abc45fd23e3362055/640ecdd4ec61394f33311a88_1_Crisp TVC on Vimeo-transcode.mp4,https://assets-global.website-files.com/640d124abc45fd23e3362055/640ecdd4ec61394f33311a88_1_Crisp TVC on Vimeo-transcode.webm" data-autoplay="true" data-loop="true" data-wf-ignore="true" class="background-video no-image w-background-video w-background-video-atom"><video id="" autoplay="" loop="" style="background-image:url(&quot;https://assets-global.website-files.com/640d124abc45fd23e3362055/640ecdd4ec61394f33311a88_1_Crisp TVC on Vimeo-poster-00001.jpg&quot;)" muted="" playsinline="" data-wf-ignore="true" data-object-fit="cover"><source src="https://assets-global.website-files.com/640d124abc45fd23e3362055/640ecdd4ec61394f33311a88_1_Crisp TVC on Vimeo-transcode.mp4" data-wf-ignore="true"><source src="https://assets-global.website-files.com/640d124abc45fd23e3362055/640ecdd4ec61394f33311a88_1_Crisp TVC on Vimeo-transcode.webm" data-wf-ignore="true"></video>
                     </div> -->
 
+            <div id="project-hero-b" class="project-hero-b">
 
-            <div id="" data-w-id="" style="width: 100%;" class="project-hero-b">
                 <div style="transform: translate3d(0%, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;" class="block-project-hero">
-                    <video src="" controls autoplay>
-                    <source src="https://www.youtube.com/embed/V4ulYUEyPXk?si=QqT0V_3qvof9TjxB" type="video/mp4">
-                    </video>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/V4ulYUEyPXk?si=QqT0V_3qvof9TjxB" title="YouTube video player" frameborder="0" rel="0" autoplay=1 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </div>
 
                 <div style="transform: translate3d(0%, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;" class="block-project-hero">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/W1KC7sao8Go?si=MCGgTHePrlazo2ap" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/W1KC7sao8Go?si=MCGgTHePrlazo2ap" title="YouTube video player" frameborder="0" autoplay=1 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </div>
-                <div style="transform: translate3d(0%, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;" class="block-project-hero">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/oOC2Ag4bzTk?si=Ni58C3BOepjHkLt4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                </div>
-                <div style="transform: translate3d(0%, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;" class="block-project-hero">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/qhNrG_pxR94?si=9iiFPmToyooR9Oj1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                </div>
-                <div style="transform: translate3d(0%, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;" class="block-project-hero"></div>
 
+                <div style="transform: translate3d(0%, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;" class="block-project-hero">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/oOC2Ag4bzTk?si=Ni58C3BOepjHkLt4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+
+                <div style="transform: translate3d(0%, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;" class="block-project-hero">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/qhNrG_pxR94?si=9iiFPmToyooR9Oj1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                
             </div>
         </main>
 
